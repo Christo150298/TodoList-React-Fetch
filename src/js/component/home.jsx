ea343b9c1992c0066ebdb3c4cc6563d6c9b5863a
Christopher Álvarez <christopherjavier1998@gmail.com>
2023-03-08 17:29:48 +0000

    avances

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,19 +3,27 @@ import { useState } from "react";
 
 const Home = () => {
 
-const [inputValue, setInputValue] = useState('');
+const [inputValue, setInputValue] = useState("");
+const [todos, setTodos] = useState([])
+
 const handleKeyDown = (e) => {
-		if (e.key === 'Enter');
+	if(e.key === "Enter") {
+	setTodos(todos.concat(inputValue));
+	setInputValue("");
+	}
 };
 
-const validateInput = () => {
-	if(inputValue === "");
-}
 	return (
 		<div className="container">
 			<h1>To do List</h1>
-			<input type="text" placeholder="What needs to be done?" 
-			value={inputValue} onChange={e => setInputValue(e.target.value)} onKeyDown={handleKeyDown} />
+			<ul>
+				<li>
+					<input type="text" placeholder="What needs to be done?" 
+					value={inputValue} onChange={e => setInputValue(e.target.value)} onKeyDown={handleKeyDown} />
+				</li>
+				{todos.map((t) => (<li>{t} <i class="fa-solid fa-x"></i> </li>))}
+			</ul>
+			<div className="footer">23 Tasks</div>
 		</div>
 	);
 };
